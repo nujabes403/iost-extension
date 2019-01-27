e89f9b7d46a75cd4f41622ffc46a49f7e38bc0f4
nujabes403 <nujabes403@gmail.com>
2019-01-28 01:47:43 +0900

    Add QR code page

--- src/components/Account.js
+++ src/components/Account.js
@@ -6,6 +6,7 @@ import Button from 'components/Button'
 import TokenBalance from 'components/TokenBalance'
 import TokenTransfer from 'components/TokenTransfer'
 import TokenSelector from 'components/TokenSelector'
+import AccountQRCode from 'components/AccountQRCode'
 import ui from 'utils/ui'
 
 import './Account.scss'
@@ -31,7 +32,14 @@ class Account extends Component<Props> {
       <div className="Account">
         <div className="Account__header">
           <p className="Account__headerTitle">Account</p>
-          <p className="Account__accountName">{iost.account.getID()}</p>
+          <p
+            className="Account__accountName"
+            onClick={() => ui.openPopup({
+              content: <AccountQRCode value={iost.account.getID()} />,
+            })}
+          >
+            {iost.account.getID()}
+          </p>
           <Button
             className={cx('Account__controlButton', 'Account__controlButton--setting')}
             onClick={this.moveToSetting}
