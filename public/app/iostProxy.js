3e3e7c25cba01ff57a765decfe09a1544339e7b5
tonokim <tonokim@qq.com>
2019-03-15 17:52:35 +0800

    fixed before window close

--- public/app/iostProxy.js
+++ public/app/iostProxy.js
@@ -127,7 +127,7 @@ function signAndSend(tx){
       txABI: [action.contract, action.actionName, JSON.parse(action.data)]
     }
   }
-  console.log(message)
+  // console.log(message)
   actionMap[actionId] = cb
   window.postMessage(message, '*')
   return cb
